ab9e9a3a7633c43e756022e80e621c09a25c60a6
Vladislav Savenko <vladislav.savenko1996@gmail.com>
2025-06-17 19:02:28 +0300

    implemented calculator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Сумма: " + (firstNumber + secondNumber));
+        System.out.println("Разность: " + (firstNumber - secondNumber));
+        System.out.println("Произведение: " + firstNumber * secondNumber);
+        System.out.println("Частное: " + (double) firstNumber / secondNumber);
     }
 }
