2b296f81bf67d0178863d5dd4763ad112e1cca0a
Vladislav Savenko <vladislav.savenko1996@gmail.com>
2025-07-09 00:47:45 +0300

    implemented main parser logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int count = 0;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExists || !isDirectory) {
+                System.out.println("Указанный файл не существует\n" +
+                        "или указанный путь является путём к папке, а не к файлу");
+                continue;
+            }
+            count++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + count);
+        }
     }
 }
