dde494686f428f5d76013fed420d7a28e3f7b035
Vladislav Savenko <vladislav.savenko1996@gmail.com>
2025-08-13 17:59:27 +0300

    Курсовой проект по теме ООП. Задание #2. Сравнение объектов - implemented calculation of the share of bot names

--- src/Main.java
+++ src/Main.java
@@ -17,35 +17,59 @@ public class Main {
             count++;
             System.out.println("Путь указан верно");
             System.out.println("Это файл номер " + count);
-            int countLines = 0, maxLength = 0, minLength = 0;
+            int countLines = 0, googleBotCount = 0, yandexBotCount = 0;
             try {
                 FileReader fileReader = new FileReader(path);
                 BufferedReader reader = new BufferedReader(fileReader);
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    if (countLines == 0){
-                        minLength = line.length();
-                    }
                     countLines++;
                     int length = line.length();
                     if (length > 1024) {
                         throw new LongLineException("В файле содержится строка длиннее 1024 символов. Строка номер: "
                                 + countLines);
                     }
-                    if (length > maxLength) {
-                        maxLength = length;
-                    }
-                    if (length < minLength) {
-                        minLength = length;
+                    String userAgent = getUserAgent(line);
+                    if (userAgent != null) {
+                        String botName = getBotNameFromUserAgent(userAgent);
+                        if ("Googlebot".equals(botName)) {
+                            googleBotCount++;
+                        } else if ("YandexBot".equals(botName)) {
+                            yandexBotCount++;
+                        }
                     }
                 }
 
                 System.out.println("Общее количество строк в файле: " + countLines);
-                System.out.println("Длина самой длинной строки в файле: " + maxLength);
-                System.out.println("Длина самой короткой строки в файле: " + minLength);
+                System.out.printf("Доля запросов Googlebot в процентах от общего количество строк в файле: %.2f\n", (googleBotCount != 0 ? (googleBotCount * 100.0 / countLines) : 0));
+                System.out.printf("Доля запросов YandexBot в процентах от общего количество строк в файле: %.2f\n", (yandexBotCount != 0 ? (yandexBotCount * 100.0 / countLines) : 0));
+
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
+    }
+
+    private static String getUserAgent(String line) {
+        String[] parts = line.split("\"");
+        String userAgent = parts[parts.length - 1];
+        if (userAgent.startsWith("Mozilla") && userAgent.contains("compatible")) {
+            return userAgent;
+        }
+        return null;
+    }
+
+    private static String getBotNameFromUserAgent(String userAgent) {
+        int startFirstBrackets = userAgent.indexOf('(');
+        int endFirstBrackets = userAgent.indexOf(')');
+        if (startFirstBrackets != -1 && endFirstBrackets != -1 && startFirstBrackets < endFirstBrackets) {
+            String firstBrackets = userAgent.substring(startFirstBrackets + 1, endFirstBrackets);
+            String[] parts = firstBrackets.split(";");
+            if (parts.length >= 2) {
+                String fragment = parts[1].trim();
+                return fragment.split("/")[0];
+            }
+        }
+        return null;
     }
 }
